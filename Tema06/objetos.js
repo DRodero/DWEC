000815e35ce24a5abdd04f41fdc30bfecaccc455
Diego <diego@rodero.info>
2025-10-13 17:46:14 +0200

    Ejemplos y ejercicios Tema 6

--- Tema06/objetos.js
+++ Tema06/objetos.js
@@ -28,6 +28,39 @@ window.addEventListener('load', function () {
     const main = document.getElementById('main');
     main.appendChild(info);
 
+
+    // Ejemplo de creación de objetos con dos sintaxis diferentes
+    // Usando llaves {}
+    let persona = {
+        nombre: 'Ana',
+        apellidos: 'López Sánchez',
+        edad: 28
+    }
+
+    // Usando new Object()
+    let persona2 = new Object();
+    persona2.nombre = 'Luis';
+    persona2.apellidos = 'Pérez Gómez';
+    persona2.edad = 35;
+
+    // Ejemplo de borrado de la proiedad edad
+    delete persona2.edad;
+
+    console.log(persona);
+    console.log(persona2);
+
+    // Ejemplo de función dentro de un objeto
+    let coche = {
+        marca: 'Renault',
+        modelo: 'Austral',
+        año: 2022,
+        mostrarInfo: function () {
+            return `Coche: ${this.marca} ${this.modelo}, Año: ${this.año}`;
+        }
+    }
+
+    console.log(coche.mostrarInfo());
+
 });
 
 
